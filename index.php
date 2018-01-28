8fcd991c0e19f792fb876aaea18a5b3c2e906575
Andy Williams <andy@andywilliams.me>
2018-01-28 20:31:43 +0000

    First commit of a project list/vote PHP app

---
+++ index.php
@@ -0,0 +1,117 @@
+<html>
+  <head>
+    <title>ajwillia.ms Project List</title>
+
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://use.fontawesome.com/c280c4d9a4.js"></script>
+
+  <link rel="stylesheet" id="all-css-2-1" href="https://s1.wp.com/_static/??-eJyFkNFuwjAMRX8I45WJipeJb0mDCUZJHDku1f5+GX1YB6K8+cr32NfGqYCXbJQN0wgljoFzxUCZlFvjRbn1tW5wwdqFElUs44CTsilW+4703tYMOPGJ9L2Vs5+nwlS8pCdgEb9NDGQVq3h2Eeb0S7EGZ7G2150SZxicwm235g4kEMU7Y8n/BJyj4+ezFqjSECW0Mtzf8CfXIJMKSkXU4CyaHvUvekxf3b777Lv+cPi4/gDGL7UC?cssminify=yes" type="text/css" media="all">
+<style type="text/css" id="custom-colors-css">span.infinite-loader .spinner div div{background:#303030 !important}body{background-color:#303030}.jetpack-recipe-title{border-color:#e74c3c}.jetpack-recipe{border-color:#e74c3c}.jetpack-recipe{border-color:rgba(231,76,60,.15)}cite,.widget-area button,.widget-area html input[type="button"],.widget-area input[type="reset"],.widget-area input[type="submit"],a,.social-links a,.entry-title a:hover,.entry-title a:focus,.entry-title a:active,.entry-meta a:hover,.entry-meta a:focus,.entry-meta a:active,#content [class*="navigation"] a:hover,#content [class*="navigation"] a:focus,#content [class*="navigation"] a:active,.comment-metadata a:hover,.comment-metadata a:focus,.comment-metadata a:active,.widget_tag_cloud a:hover,.widget_tag_cloud a:focus,.widget_tag_cloud a:active,.content-area .widget_categories ul a:hover,.content-area .widget_recent_entries ul a:hover,.widget-grofile .grofile-full-link,.widget-grofile .grofile-full-link:hover,.widget-grofile .grofile-full-link:focus,.widget-grofile .grofile-full-link:active,.custom-colors .wp_widget_tag_cloud a:hover,.custom-colors .wp_widget_tag_cloud a:focus,.custom-colors .wp_widget_tag_cloud a:active{color:#e74c3c}mark,ins,button,html input[type="button"],input[type="reset"],input[type="submit"],body:after,.site-header,.main-navigation ul ul,.more-link,.wp-audio-shortcode .mejs-controls .mejs-time-rail .mejs-time-current,.wp-audio-shortcode .mejs-controls .mejs-horizontal-volume-slider .mejs-horizontal-volume-current,.content-area .widget_tag_cloud a,#infinite-handle span{background-color:#e74c3c}a:hover,a:focus,a:active,.site-title a:hover,.site-info a:hover,.widget_archive ul a:hover,.widget_categories ul a:hover,.widget_links ul a:hover,.widget_nav_menu ul a:hover,.widget_meta ul a:hover,.widget_pages ul a:hover,.widget_recent_comments ul a:hover,.widget_recent_entries ul a:hover,.widget_rss ul a:hover,.widget_rss_links ul a:hover,.custom-colors .widget_top-posts ul a:hover{color:#c0392b}button:hover,html input[type="button"]:hover,input[type="reset"]:hover,input[type="submit"]:hover,button:focus,html input[type="button"]:focus,input[type="reset"]:focus,input[type="submit"]:focus,button:active,html input[type="button"]:active,input[type="reset"]:active,input[type="submit"]:active,#sidebar-toggle,.main-navigation a:hover,.menu-toggle,.dropdown-icon,.social-links a,.more-link:hover,.more-link:focus,.more-link:active,.widget th,.widget tfoot tr,.widget_tag_cloud a,.content-area .widget_tag_cloud a:hover,.content-area .widget_tag_cloud a:focus,.content-area .widget_tag_cloud a:active,#infinite-handle span:hover,.custom-colors .wp_widget_tag_cloud a{background-color:#c0392b}.widget-area input[type="text"],.widget-area input[type="url"],.widget-area input[type="email"],.widget-area input[type="password"],.widget-area input[type="search"],.widget-area textarea{border-color:#c0392b}#sidebar-toggle:before,#sidebar-toggle:after{border-top-color:#c0392b}.site-branding,.main-navigation:after,#social-links,.widget,.widget th,.widget td{border-bottom-color:#c0392b}</style>
+
+  </head>
+  <body>
+    <div id="page">
+<div id="sidebar" class="sidebar-area">
+	<a id="sidebar-toggle" href="#" title="Sidebar"><span class="genericon genericon-close"></span><span class="screen-reader-text">Sidebar</span></a><header id="masthead" class="site-header" role="banner" style="display: block;">
+					<a class="site-logo" href="https://ajwillia.ms/" title="Andy Williams" rel="home">
+				<img src="https://secure.gravatar.com/avatar/b76268f3a255915fa380139dac950652/?s=120&amp;d=identicon" width="120" height="120" alt="" class="no-grav header-image" originals="120" scale="1.5">
+			</a>
+				<div class="site-branding">
+			<h1 class="site-title"><a href="https://ajwillia.ms/" title="Andy Williams" rel="home">Andy Williams</a></h1>
+			<h2 class="site-description">Ramblings of a guy playing with code and trains</h2>
+		</div>
+
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+			<h1 class="menu-toggle genericon genericon-menu"><span class="screen-reader-text">Menu</span></h1>
+			<div class="screen-reader-text skip-link"><a href="#content" title="Skip to content">Skip to content</a></div>
+			<div class="menu-menu-1-container"><ul id="menu-menu-1" class="menu nav-menu"><li class=" menu-item menu-item-type-custom menu-item-object-custom menu-item-home"><a href="https://ajwillia.ms/">Home</a></li><li class=" menu-item menu-item-type-post_type menu-item-object-page"><a href="https://ajwillia.ms/about/">About</a></li><li class=" menu-item menu-item-type-post_type menu-item-object-page"><li class=" menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home"><a href="https://ajwillia.ms/projects/">Projects</a></li><a href="https://ajwillia.ms/contact/">Contact</a></li></ul></div>		</nav><!-- #site-navigation -->
+			</header>
+</div>
+
+      <div id="content" class="site-content"><div id="primary" class="content-area">
+
+      <header class="entry-header">
+        <h1 class="entry-title">Project List</h1>
+        <span class="entry-format-badge genericon genericon-document"><span class="screen-reader-text">Standard</span></span>
+
+        <br />
+        <p>Help me decide which projects should be progressed next - this list is ordered by popularity so I will try to work on the items at the top first.</p>
+      </header>
+
+<?php if ($_GET['vote'] != '') { ?>
+    <div class="alert alert-danger" role="alert">
+      This has not been implemented :( !
+    </div>
+<?php } ?>
+
+    <ul class="list-group">
+<?php
+
+$colors = Array();
+function category_color($category) {
+  global $colors;
+
+  if ($colors[$category])
+    return $colors[$category];
+
+  $color = 'badge-light';
+  switch (count($colors)) {
+  case 0:
+    $color = 'badge-primary';
+    break;
+  case 1:
+    $color = 'badge-success';
+    break;
+  case 2: 
+    $color = 'badge-warning';
+    break;
+  case 3: 
+    $color = 'badge-danger';
+    break;
+  case 4: 
+    $color = 'badge-info';
+    break;
+  }
+
+  $colors[$category] = $color;
+  return $color;
+}
+
+$db = new SQLite3('projects.sqlite');
+
+$results = $db->query('SELECT projects.rowid, title, category, description, COALESCE(votes, 0) AS votes '.
+    'FROM projects LEFT JOIN (SELECT projectid, sum(updown) AS votes FROM votes GROUP BY projectid) on projects.rowid = projectid '.
+    'ORDER BY votes DESC, category, title');
+while ($row = $results->fetchArray()) {
+  echo '<li class="list-group-item">';
+	
+  echo '<div class="d-flex justify-content-between align-items-center">';
+  echo '  <span class="list-group-content">'.$row['title'];
+  if ($row['category'])
+    echo '    <span class="badge badge-pill '.category_color($row['category']).'">'.$row['category'].'</span>';
+  echo '  </span>';
+ 
+  if ($row['votes'] > 0)
+    echo '<span class="badge badge-pill badge-secondary">'.$row['votes'].'</span>';
+
+  echo '</div>';
+  echo '<small>'.$row['description'].'</small>';
+
+  echo '</li>';
+}
+?>
+    </ul>
+    <br />
+
+      <footer class="entry-meta">
+        <ul class="clear">
+          <li class="comment-meta">
+            <div class="genericon genericon-comment"></div>
+	    <p>If you would like me to add a project to this list please 
+            <a href="https://ajwillia.ms/contact/">get in touch</a>,</p>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  </body>
+</html>
---
+++ schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE projects(title varchar, category varchar, description varchar);
+CREATE TABLE votes(projectid int, updown int, user varchar);
